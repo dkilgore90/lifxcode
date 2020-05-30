312bf4b6c658025451f08b29aee07bafe3f5c52d
dkilgore90 <51180846+dkilgore90@users.noreply.github.com>
2020-05-30 18:22:57 -0400

    deviceSetTileEffect - generic List as input

--- LIFXMasterApp.groovy
+++ LIFXMasterApp.groovy
@@ -693,7 +693,7 @@ Map<String, List> deviceSetMultiZoneEffect(String effectType, Integer speed, Str
     actions
 }
 
-Map<String, List> deviceSetTileEffect(String effectType, Integer speed, Integer palette_count, List<Map> colors) {
+Map<String, List> deviceSetTileEffect(String effectType, Integer speed, Integer palette_count, List colors) {
     def actions = makeActions()
     Integer typeInt
     switch (effectType) {
